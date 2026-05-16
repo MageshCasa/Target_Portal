a9b5b636b5375174f64367f8b502d29ed7be2b0c
magesh <mageshwaran.p@casagrand.co.in>
2026-05-16 11:34:02 +0530

    Enhance CORS configuration to allow all origins with specified methods

--- app.js
+++ app.js
@@ -7,15 +7,27 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+// Allow All Origins
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    })
+);
+
 app.use(express.json());
 
+// Static Upload Folder
 app.use("/uploads", express.static("uploads"));
 
+// Routes
 app.use("/api", employeeRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+
     console.log(`Server running on ${PORT}`);
+
 });
